c88776d72389b8cbfec249731af8360d638e34f5
drguiritan <drguiritan@urios.edu.ph>
2025-10-05 07:43:54 +0800

    route to home page

--- birc/home/context_processors.py
+++ birc/home/context_processors.py
@@ -1,7 +1,10 @@
-from wagtail.models import Page
+from wagtail.models import Page,Site
 
 def research_links(request):
-    return {        
+    site = Site.find_for_request(request)
+    home_url = site.root_page.url if site and site.root_page else "/"
+    return {            
+        "home_url": home_url, 
         "ure_page": Page.objects.filter(slug="ure-student-research").live().first(),
         # "faculty_page": Page.objects.filter(slug="faculty-research").live().first(),
         # "collaboration_page": Page.objects.filter(slug="collaboration-research").live().first(),
